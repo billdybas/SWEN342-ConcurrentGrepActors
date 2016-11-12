87aa6f65c0a631dd0ec7de8bc76df35f98bb1762
Tina Howard <chh2538@rit.edu>
2016-11-11 19:29:05 -0500

    Made the found object

--- Found.java
+++ Found.java
@@ -1,4 +1,20 @@
+import java.util.*
 
 public class Found {
+	private ArrayList<String> foundLines;
+	private String filename
+	
+	public Found(ArrayList<String> foundLines, String filename) {
+		this.foundLines = foundLines;
+		this.filename = filename;
+	}
+	
+	public String getFilename() {
+		return filename;
+	}
+	
+	public ArrayList<String> getFoundLines() {
+		return foundLines;
+	}
 
 }
