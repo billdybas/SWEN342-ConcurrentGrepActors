8e604cb38c8e34c0eb0273d1b7b23a6e2f8dd947
Tina Howard <chh2538@rit.edu>
2016-11-11 20:34:27 -0500

    added everything but the method for filecount

--- CollectionActor.java
+++ CollectionActor.java
@@ -1,4 +1,37 @@
+import akka.actor.UntypedActor;
 
-public class CollectionActor {
+public class CollectionActor extends UntypedActor {
+	private int count = 0;
+	private int numberOfMessages
+	
+	public CollectionActor(int count, int numberOfMessages) {
+		this.numberOfMessages = numberOfMessages;
+		this.count = count;
+	}
+	
+	@Override
+	public void onReceive(Object message) {
+	
+		if(message instanceof FileCount) {
+			if(fileCount != 0) { 
+				
+			} else {
+				FileCount fileCount = (FileCount)fileCount;
+				//TODO get this thing implemented
+				count = fileCount.getNumber();
+			}
+		} else {
+			// These messages are instances of Found
+			Found found = (Found)message;
+			System.out.println(found.getFilename());
+			for(String line: found.getLines()) {
+				System.out.println(line);
+			}
+			numberOfMessages ++;
+			if(numberOfMessages == count) {
+				Actors.registry.shutDownAll();
+			}
+		}
+	}
 
 }
